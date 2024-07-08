44c15599ae51fa65988eb3fc35366fcc42b49435
Guideslop <aranmorad2024@gmail.com>
2024-07-08 22:42:22 +0200

    Create Myscript.lua

---
+++ Myscript.lua
@@ -0,0 +1,9 @@
+-- Example Lua script
+print("Hello from my script!")
+
+-- Function that runs when the script is loaded
+local function exampleFunction()
+    print("This function is running from the fetched script.")
+end
+
+exampleFunction()
